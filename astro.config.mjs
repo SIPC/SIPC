50ab52976580e9d3acba38a88f3cdd251b1f254c
ink <admin@sipc.ink>
2026-01-05 04:56:03 +0800

    v1.3
    Try to fix the avatar display issue again

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,6 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   integrations: [studiocmsUi()],
   adapter: cloudflare({
-     imageService: 'cloudflare'
+     imageService: 'passthrough'
   })
 });
