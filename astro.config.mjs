805f9d1f7ad5244ed5b55e8c38cdbfc518950819
ink <admin@sipc.ink>
2026-01-07 01:10:10 +0800

    v3.0 add i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
+// @ts-ignore
 import { defineConfig } from 'astro/config';
 
 import studiocmsUi from '@studiocms/ui';
 
+// @ts-ignore
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [studiocmsUi()],
   adapter: cloudflare({
      imageService: 'passthrough'
